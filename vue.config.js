d51e3771f84e1f36698b13c2739e69d5f95ac941
Mike K <michaelkaplan07@gmail.com>
2021-06-18 09:10:34 -0500

    changed vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,28 @@
-const webpack = require('webpack')
+// const webpack = require('webpack')
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new webpack.optimize.LimitChunkCountPlugin({
+//         maxChunks: 1
+//       })
+//     ]
+//   },
+//   chainWebpack:
+//     config => {
+//       config.optimization.delete('splitChunks')
+//     }
+// }
+
 module.exports = {
   configureWebpack: {
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1
-      })
-    ]
-  },
-  chainWebpack:
-    config => {
-      config.optimization.delete('splitChunks')
+    optimization: {
+      splitChunks: false
     }
+  }
+}
+
+module.exports = {
+  css: {
+    extract: false,
+  },
 }
