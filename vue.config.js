a98ed6d971d4c18db823db013753413f4547047d
Mike K <michaelkaplan07@gmail.com>
2021-06-18 09:42:16 -0500

    updated .toml for single .js

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,4 @@
-// const webpack = require('webpack')
-// module.exports = {
-//   configureWebpack: {
-//     plugins: [
-//       new webpack.optimize.LimitChunkCountPlugin({
-//         maxChunks: 1
-//       })
-//     ]
-//   },
-//   chainWebpack:
-//     config => {
-//       config.optimization.delete('splitChunks')
-//     }
-// }
-
+// https://stackoverflow.com/questions/52427000/vue-js-build-and-deploy-to-single-file
 module.exports = {
   css: {
     extract: false,
